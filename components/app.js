7bce3b3e4ac93f6794260c64babed27648bce85d
Victoria Corona <corona.victoria@hotmail.com>
2020-05-06 18:18:23 -0700

    success

--- components/app.js
+++ components/app.js
@@ -1,32 +1,27 @@
 class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    // this.getGrades = this.getGrades.bind(this)
+  }
   handleGetGradesError(error) {
     console.error(error);
   }
   handleGetGradesSuccess(grades) {
     console.log(grades);
   }
-  constructor() {
-    this.handleGetGradesError = this.handleGetGradesError.bind(this);
-    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
-  }
   getGrades(){
     $.ajax({
+      headers: {
+        "X-Access-Token": "jygGwmxq"
+      },
       method: "GET",
       url: "https://sgt.lfzprototypes.com/api/grades",
-      grades: function(data) {
-        console.log(data);
-      } ,
-      error: function(error){
-        console.log(error)
-        handleGetGradesError();
-      },
-      success: function(success){
-        console.log(success)
-        handleGetGradesSuccess();
-      }
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
     })
-    Start(){
-      this.getGrades();
     }
+  start() {
+    this.getGrades();
   }
 }
