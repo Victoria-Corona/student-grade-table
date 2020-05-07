0b3c88823515daf62ad51cae6846f5c2c8412641
Victoria Corona <corona.victoria@hotmail.com>
2020-05-07 12:45:12 -0700

    grades are rendered

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,8 +1,26 @@
 class GradeTable {
   constructor(tableElement){
-    this.tabletElement = tableElement;
+    this.tableElement = tableElement;
   }
   updateGrades(grades){
     console.log(grades);
+    var tbody = this.tableElement.querySelector("tbody");
+
+    for(var i = 0; i < grades.length; i++) {
+      var tableRow1 = document.createElement("tr");
+      var tableData1 = document.createElement("td");
+      var tableData2 = document.createElement("td");
+      var tableData3 = document.createElement("td");
+
+      tableData1.textContent += grades[i].name;
+      tableData2.textContent += grades[i].course;
+      tableData3.textContent += grades[i].grade;
+      tableRow1.append(tableData1);
+      tableRow1.append(tableData2);
+      tableRow1.append(tableData3);
+      tbody.append(tableRow1);
+
+    }
+
   }
 }
