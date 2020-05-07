5a092706a90924067d16235a451210076a75c2e6
Victoria Corona <corona.victoria@hotmail.com>
2020-05-07 14:43:59 -0700

    PageHeader has been instantiated

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var header = document.querySelector("header");
+var pageHeader = new PageHeader(header);
+
 var table = document.querySelector("table");
 
 var gradeTableClass = new GradeTable(table);
