97a0a3fe6e53a646e482ed6ed109d89a434815a6
Victoria Corona <corona.victoria@hotmail.com>
2020-05-06 17:24:11 -0700

    app class filled with methods

--- components/app.js
+++ components/app.js
@@ -1,1 +1,32 @@
-class App {}
+class App {
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  getGrades(){
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      grades: function(data) {
+        console.log(data);
+      } ,
+      error: function(error){
+        console.log(error)
+        handleGetGradesError();
+      },
+      success: function(success){
+        console.log(success)
+        handleGetGradesSuccess();
+      }
+    })
+    Start(){
+      this.getGrades();
+    }
+  }
+}
