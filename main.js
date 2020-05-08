9255fb8b42597f1da3b372882b980be495a44111
Victoria Corona <corona.victoria@hotmail.com>
2020-05-07 22:05:47 -0700

    app instantiatoin includes gradeform

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ var table = document.querySelector("table");
 
 var gradeTableClass = new GradeTable(table);
 
-var app = new App(gradeTableClass, pageHeader);
+var app = new App(gradeTableClass, pageHeader, gradeform);
 app.start();
