34ab53290dbfaa31db47049dd85ce48b912f03ec
Victoria Corona <corona.victoria@hotmail.com>
2020-05-10 17:45:40 -0700

    methods added and bond

--- components/app.js
+++ components/app.js
@@ -5,6 +5,9 @@ class App {
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
+    this.handleDeleteGradeSuccess = this.handleCreateGradeSuccess.bind(this);
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
     this.gradeForm = gradeForm;
@@ -67,6 +70,19 @@ class App {
   handleCreateGradeSuccess(){
     this.getGrades();
   }
+
+  deleteGrade(id){
+    console.log(id);
+  }
+
+  handleDeleteGradeError(error){
+    console.error(error);
+  }
+
+  handleDeleteGradeSuccess(){
+    this.getGrades();
+  }
+
 }
 
 //make the placeholder for the sum of everything
